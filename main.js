318e12f7f85ef09576469e2eab5aae43955e50b0
Proxtx <39201896+Proxtx@users.noreply.github.com>
2022-12-26 13:10:01 +0100

    working

---
+++ main.js
@@ -0,0 +1,48 @@
+import { registerAppEvent } from "../../private/playbackLoader.js";
+import { genCombine } from "@proxtx/combine-rest/request.js";
+import { genModule } from "@proxtx/combine/combine.js";
+
+export class App {
+  updateCheckInterval = 1 * 60 * 1000;
+  lastNotes;
+
+  constructor(config) {
+    this.config = config;
+    (async () => {
+      this.notesApi = await genCombine(
+        this.config.apiUrl,
+        "public/notes.js",
+        genModule
+      );
+
+      this.mainUrl = new URL(this.config.apiUrl);
+      this.mainUrl.pathname = "/";
+      this.mainUrl = this.mainUrl.href;
+
+      while (true) {
+        (async () => {
+          try {
+            await this.checkForNewNotes();
+          } catch (e) {
+            console.log(e);
+          }
+        })();
+        await new Promise((r) => setTimeout(r, this.updateCheckInterval));
+      }
+    })();
+  }
+
+  async checkForNewNotes() {
+    let notes = await this.notesApi.getNotes(this.config.pwd);
+    if (notes != this.lastNotes)
+      registerAppEvent({
+        app: "Notes",
+        type: "Notes Changed",
+        text: `${notes}`,
+        media: [],
+        time: Date.now(),
+        open: this.mainUrl,
+        points: this.config.points,
+      });
+  }
+}
